232bafe91a03196d8d1bcf72f59a660943e28d13
Des <desmond@fastmail.com>
2023-03-26 18:26:24 -0700

    fix note octave values

--- src/utils/notes.ts
+++ src/utils/notes.ts
@@ -3,77 +3,77 @@ import { INote } from "./types";
 export const naturals: INote[] = [
     {
         note: "E",
-        octave: 2,
+        octave: 1,
         notation: "E,,",
     },
     {
         note: "F",
-        octave: 2,
+        octave: 1,
         notation: "F,,",
     },
     {
         note: "G",
-        octave: 2,
+        octave: 1,
         notation: "G,,"
     },
     {
         note: "A",
-        octave: 2,
+        octave: 1,
         notation: "A,,"
     },
     {
         note: "B",
-        octave: 2,
+        octave: 1,
         notation: "B,,"
     },
     {
         note: "C",
-        octave: 3,
+        octave: 2,
         notation: "C,"
     },
     {
         note: "D",
-        octave: 3,
+        octave: 2,
         notation: "D,"
     },
     {
         note: "E",
-        octave: 3,
+        octave: 2,
         notation: "E,"
     },
     {
         note: "F",
-        octave: 3,
+        octave: 2,
         notation: "F,"
     },
     {
         note: "G",
-        octave: 3,
+        octave: 2,
         notation: "G,"
     },
     {
         note: "A",
-        octave: 3,
+        octave: 2,
         notation: "A,"
     },
     {
         note: "B",
-        octave: 3,
+        octave: 2,
         notation: "B,"
     },
     {
         note: "C",
-        octave: 4,
+        octave: 3,
         notation: "C"
     },
     {
         note: "D",
-        octave: 4,
+        octave: 3,
         notation: "D"
     },
     {
         note: "E",
-        octave: 4,
+        octave: 3,
         notation: "E"
     },
 ];
@@ -81,52 +81,52 @@ export const naturals: INote[] = [
 export const sharps: INote[] = [
     {
         note: "F♯",
-        octave: 2,
+        octave: 1,
         notation: "^F,,",
     },
     {
         note: "G♯",
-        octave: 2,
+        octave: 1,
         notation: "^G,,",
     },
     {
         note: "A♯",
-        octave: 2,
+        octave: 1,
         notation: "^A,,",
     },
     {
         note: "A♯",
-        octave: 3,
+        octave: 2,
         notation: "^A,",
     },
     {
         note: "F♯",
-        octave: 3,
+        octave: 2,
         notation: "^F,",
     },
     {
         note: "D♯",
-        octave: 3,
+        octave: 2,
         notation: "^D,",
     },
     {
         note: "D♯",
-        octave: 4,
+        octave: 3,
         notation: "^D",
     },
     {
         note: "G♯",
-        octave: 3,
+        octave: 2,
         notation: "^G,",
     },
     {
         note: "C♯",
-        octave: 4,
+        octave: 3,
         notation: "^C",
     },
     {
         note: "C♯",
-        octave: 3,
+        octave: 2,
         notation: "^C,",
     },
 ];
@@ -134,52 +134,52 @@ export const sharps: INote[] = [
 export const flats: INote[] = [
     {
         note: "G♭",
-        octave: 2,
+        octave: 1,
         notation: "_G,,",
     },
     {
         note: "A♭",
-        octave: 2,
+        octave: 1,
         notation: "_A,,",
     },
     {
         note: "B♭",
-        octave: 2,
+        octave: 1,
         notation: "_B,,",
     },
     {
         note: "B♭",
-        octave: 3,
+        octave: 2,
         notation: "_A,",
     },
     {
         note: "G♭",
-        octave: 3,
+        octave: 2,
         notation: "_G,",
     },
     {
         note: "E♭",
-        octave: 3,
+        octave: 2,
         notation: "_E,",
     },
     {
         note: "E♭",
-        octave: 4,
+        octave: 3,
         notation: "_E",
     },
     {
         note: "A♭",
-        octave: 3,
+        octave: 2,
         notation: "_A,",
     },
     {
         note: "D♭",
-        octave: 4,
+        octave: 3,
         notation: "_D",
     },
     {
         note: "D♭",
-        octave: 3,
+        octave: 2,
         notation: "_D,",
     },
 ];
